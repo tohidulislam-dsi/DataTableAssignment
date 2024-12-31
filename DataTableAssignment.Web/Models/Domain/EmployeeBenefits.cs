6dcbafeb3a280765ffa4067eef83b1d78ec8e439
Md Tohidul Islam <tohidul.islam@dsinnovators.com>
2024-12-31 20:25:34 +0600

    refactored

--- DataTableAssignment.Web/Models/Domain/EmployeeBenefits.cs
+++ DataTableAssignment.Web/Models/Domain/EmployeeBenefits.cs
@@ -2,7 +2,7 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace DataTableAssignment.Web.Models
+namespace DataTableAssignment.Web.Models.Domain
 {
     public class EmployeeBenefits
     {
--- DataTableAssignment.Web/Models/ViewModel/EmployeeViewModel.cs
+++ DataTableAssignment.Web/Models/ViewModel/EmployeeViewModel.cs
@@ -20,5 +20,18 @@ namespace DataTableAssignment.Web.Models.ViewModel
         [Required(ErrorMessage = "This field is required")]
         [Range(0, int.MaxValue, ErrorMessage = "Salary cannot be negative")]
         public int Salary { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        public string BenefitType { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        [Range(0, int.MaxValue, ErrorMessage = "Benefit value cannot be negative")]
+        public int BenefitValue { get; set; }
     }
 }
