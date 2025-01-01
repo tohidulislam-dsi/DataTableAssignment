b54e6a7a2f3d20c91ebfea3fb9bdc4b84555ee03
Md Tohidul Islam <tohidul.islam@dsinnovators.com>
2025-01-01 18:59:56 +0600

    fixed sql scrtipt issue

--- DataTableAssignment.Web/SqlScripts/GetEmployeeDetailsByEmployeeId.sql
+++ DataTableAssignment.Web/SqlScripts/GetEmployeeDetailsByEmployeeId.sql
@@ -14,20 +14,3 @@ BEGIN
     WHERE
         EmployeeId = @EmployeeId;
 END;
-
-CREATE OR ALTER PROCEDURE GetEmployeeBenefitsByEmployeeDetailsId
-    @EmployeeDetailId UNIQUEIDENTIFIER
-AS
-BEGIN
-    SET NOCOUNT ON;
-
-    SELECT
-        Id,
-        EmployeeDetailId,
-        BenefitType,
-        BenefitValue
-    FROM
-        EmployeeBenefits
-    WHERE
-        EmployeeDetailId = @EmployeeDetailId;
-END;
